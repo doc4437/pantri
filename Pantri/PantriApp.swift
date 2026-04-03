8861e880a8ff1d416fb273a882466fb97eae5736
Brad D. Faglie, MD <doc@ensodirectcr>
2026-04-02 20:43:26 -0500

    Initial Commit

---
+++ Pantri/PantriApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantriApp.swift
+//  Pantri
+//
+//  Created by Enso Direct Care on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PantriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
